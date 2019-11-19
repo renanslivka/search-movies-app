353efb1ec85e0c8fb2343ac3c5797618150f7047
renanslivka <renanslivka@gmail.com>
2019-11-19 21:31:42 +0200

    Updates

---
+++ precache-manifest.830041b656d8d400517271ec7b5bff98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5beed063fd87935a0af5e38403c96d5",
+    "url": "/search-movies-app/index.html"
+  },
+  {
+    "revision": "be01848bd6d16ce08661",
+    "url": "/search-movies-app/static/css/main.60fb755a.chunk.css"
+  },
+  {
+    "revision": "ca0f8f8668cb8b87a87f",
+    "url": "/search-movies-app/static/js/2.edc806f2.chunk.js"
+  },
+  {
+    "revision": "be01848bd6d16ce08661",
+    "url": "/search-movies-app/static/js/main.eaa671b1.chunk.js"
+  },
+  {
+    "revision": "c4d7c6fd28a30f32b3f7",
+    "url": "/search-movies-app/static/js/runtime-main.ddb5c7a4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/search-movies-app/static/media/logo.25bf045c.svg"
+  }
+]);
